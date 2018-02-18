795c5b2aeb7ae6472d9c247119279dd5dbb1f896
surjitprafullit <32036204+surjitprafullit@users.noreply.github.com>
2018-02-18 23:11:29 +0530

    Update 1.c

--- Arrays/1.c
+++ Arrays/1.c
@@ -1,9 +1,14 @@
-/*Date:18/02/2018
+/* Date:18/02/2018
+
 Problem: Reverse the array.
+
 Problem url:https://www.hackerrank.com/challenges/arrays-ds/problem
+
 Input: Length and elements of the array.
+
 How to solve: Go to the last element and traverse backwards while printing each element seperated by a space.
 */
+
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
@@ -18,3 +23,10 @@ int main(){
     }
     return 0;
 }
+/*Result: Input (stdin)
+          4
+          1 4 3 2
+          Output (stdout)
+          2 3 4 1 
+  Learnings: Dynamic initialization of array, using malloc().
+ */
