d8eac57d5b29b37822b6f61afc02af86e57b69f4
surjitprafullit <32036204+surjitprafullit@users.noreply.github.com>
2018-03-23 15:50:51 +0530

    Create 3.c

---
+++ Arrays/3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int max(int a, int b) {
+	if (a >= b)
+		return (a);
+	else
+		return (b);
+}
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n, i;
+		scanf("%d", &n);
+		int *arr = (int*) (malloc(sizeof(int) * n));
+		for (i = 0; i < n; i++) {
+			scanf("%d", &arr[i]);
+		}
+		int max_current = arr[0];
+		int max_global = arr[0];
+		for (i = 1; i < n; i++) {
+			max_current = max(arr[i], max_current + arr[i]);
+			if (max_current > max_global)
+				max_global = max_current;
+
+		}
+		printf("%d\n", max_global);
+	}
+	return (0);
+}
+
